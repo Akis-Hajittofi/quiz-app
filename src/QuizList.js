3fbbdc113467fd6fcfadf0a9a3864d8f52e9697e
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-05-18 21:29:01 +0100

    Added a random quiz button

--- src/QuizList.js
+++ src/QuizList.js
@@ -1,12 +1,30 @@
 import React from "react";
 import QuizRow from "./QuizRow";
 import "./QuizList.css";
+import QuizIcon from "@mui/icons-material/Quiz";
+import { useNavigate } from "react-router-dom";
 
 function QuizList() {
   const list = require("./data/list/list.json");
+  const navigate = useNavigate();
+
+  const randomQuiz = () => {
+    const randomisedQuiz =
+      list.data[Math.floor(Math.random() * list.data.length)];
+
+    console.log("RANDOM: ", randomisedQuiz);
+
+    navigate(`/quiz-${randomisedQuiz.quizType}`, {
+      state: { quiz: randomisedQuiz.fileName },
+    });
+  };
 
   return (
     <div className="quizList">
+      <div onClick={randomQuiz} className="quizList__random">
+        <QuizIcon className="quizList__randomIcon" />
+        <h3>Random</h3>
+      </div>
       {list.data.map((quiz) => (
         <QuizRow
           name={quiz.name}
