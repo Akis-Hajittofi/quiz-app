175773a842310b368b5ab909b4a5a757f5cba396
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2025-01-27 22:38:53 +0000

    Add pagination for leaderboard by QuizID
    - Added page and limit query parameters to handle pagination
    - Included Metadata: currentyPage, totalRows and totalPages
    - Validated page and limit for positive values

--- backend/src/controllers/leaderboardsController.ts
+++ backend/src/controllers/leaderboardsController.ts
@@ -63,17 +63,49 @@ export const getAllLeaderboards = async (req: Request, res: Response) => {
 // GET leaderboard by Quiz ID
 export const getLeaderboardByQuizID = async (req: Request, res: Response) => {
   const { QuizID } = req.params;
+  const { page, limit } = req.query;
+
+  // Convert query params to numbers
+  const pageNumber = parseInt(page as string, 10) || 1;
+  const pageLimit = parseInt(limit as string, 10) || 10;
+
+  if (pageNumber <= 0 || pageLimit <= 0) {
+    return res.status(400).json({ error: "Invalid page or limit." });
+  }
 
   try {
-    const [leaderboard] = await pool.query<RowDataPacket[]>(
-      "SELECT * FROM leaderboards WHERE QuizID = ? ORDER BY ScorePercentage DESC",
+    const [totalRowsResult] = await pool.query<RowDataPacket[]>(
+      "SELECT COUNT(*) as total FROM leaderboards WHERE QuizID = ?",
       [QuizID]
     );
+    const totalRows = totalRowsResult[0].total;
 
-    if (leaderboard.length === 0) {
-      return res.status(404).json({ error: "Leaderboard not found" });
+    if (totalRows === 0) {
+      return res.status(404).json({ error: "Leaderboard not found." });
     }
-    res.json(leaderboard);
+
+    const totalPages = Math.ceil(totalRows / pageLimit);
+
+    if (pageNumber > totalPages) {
+      return res.status(404).json({ error: "Page invalid." });
+    }
+
+    const offset = (pageNumber - 1) * pageLimit;
+
+    const [leaderboard] = await pool.query<RowDataPacket[]>(
+      "SELECT * FROM leaderboards WHERE QuizID = ? ORDER BY ScorePercentage DESC LIMIT ? OFFSET ?",
+      [QuizID, pageLimit, offset]
+    );
+
+    res.json({
+      data: leaderboard,
+      meta: {
+        currentPage: pageNumber,
+        pageSize: pageLimit,
+        totalRows,
+        totalPages,
+      },
+    });
   } catch (error) {
     console.error("Database error:", error);
     res.status(500).json({ error: "Internal Server Error" });
